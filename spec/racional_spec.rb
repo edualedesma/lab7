31f34554260345a3353dfcf149bdc45560de8f4d
alu3286 <Edinho@MacBook-Pro-Edinho.local>
2012-10-31 14:58:44 +0000

    Creacion de ficheros necesarios

---
+++ spec/racional_spec.rb
@@ -0,0 +1,11 @@
+require 'racional'
+
+describe Racional do
+	before :each do
+		@racional = Racional.new()
+	end
+	
+	it "Debe existir un numerador" do
+		#@racional.numerador.should == 
+	end
+end
