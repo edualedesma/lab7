587df869928e336c0b6d399d00164f1ad8cef650
alu3286 <Edinho@MacBook-Pro-Edinho.local>
2012-11-02 18:28:52 +0000

    añadidas varias funcionalidades

--- lib/racional.rb
+++ lib/racional.rb
@@ -1,3 +1,39 @@
 class Racional
+	
+	attr_accessor :numerador, :denominador
+
+	def initialize(num=4, den=3)
+		@numerador = num
+		@denominador = den
+	end
+	
+	def num
+		@numerador
+	end
+	
+	def denom
+		@denominador
+	end
+	
+	def mostrar
+		num.to_s + "/" + denom.to_s
+	end
+	
+	def mostrar_flotante
+		(num.to_f/denom.to_f)
+	end
+	
+	def ==(o)
+		return @numerador == o.num && @denominador == o.denom if o.instance_of? Racional
+		false
+	end
+	
+	#def abs(o)
+	#	if o.num < 0
+	#		o.num = - o.num
+	#	if o.denom < 0
+	#		o.denom = - o.denom
+	#	retur o
+	#end
 
 end
--- spec/racional_spec.rb
+++ spec/racional_spec.rb
@@ -6,6 +6,39 @@ describe Racional do
 	end
 	
 	it "Debe existir un numerador" do
-		#@racional.numerador.should == 
+		@racional.numerador.should == 4
+	end
+	
+	it "Debe existir un denominador" do
+		@racional.denominador.should == 3
+	end
+	
+	#it "Debe de estar en su forma reducida" do
+		#????????
+	#end
+	
+	it "Se debe invocar al metodo num() para obtener el numerador" do
+		@racional.num
+	end
+	
+	it "Se debe invocar al metodo denom() para obtener el denominador" do
+		@racional.denom
+	end
+	
+	it "Se debe mostar por la consola la fraccion de la forma: a/b, 
+		donde a es el numerador y b el denominador" do
+		@racional.mostrar
+	end
+	
+	it "Se debe mostar por la consola la fraccion en formato flotante" do
+		@racional.mostrar_flotante
+	end
+	
+	it "Se debe comparar si dos fracciones son iguales con ==" do
+		@racional.== Racional.new(4,3)
+	end
+	
+	it "Se debe calcular el valor absoluto de una fraccion con el metodo abs" do
+		#@racional.abs Racional.new(2,3)
 	end
 end
