a769cc8d7600828419f4c8d7a5a8257079cca277
alu3286 <Edinho@MacBook-Pro-Edinho.local>
2012-12-02 14:48:59 +0000

    modificacion de la clase y pruebas spec

--- spec/racional_spec.rb
+++ spec/racional_spec.rb
@@ -21,11 +21,11 @@ describe Racional do
 	end
 	
 	it "Se debe invocar al metodo num() para obtener el numerador" do
-		@racional.numerador.should == 0
+		Racional.new(1, 5).respond_to?("numerador").should == true
 	end
 	
 	it "Se debe invocar al metodo denom() para obtener el denominador" do
-		@racional.denominador.should == 1
+		Racional.new(1, 5).respond_to?("denominador").should == true
 	end
 	
 	it "Se debe mostar por la consola la fraccion de la forma: a/b, donde a es el numerador y b el denominador" do
@@ -33,16 +33,16 @@ describe Racional do
 	end
 	
 	it "Se debe mostar por la consola la fraccion en formato flotante" do
-		@racional.mostrar_flotante
+		@racional.to_f.should == 0/1
+        @racional2.to_f.should == 2/4
 	end
 	
 	it "Se debe comparar si dos fracciones son iguales con ==" do
-		@racional.== Racional.new(4,3)
+		@racional2.should == Racional.new(1,2)
 	end
 	
 	it "Se debe calcular el valor absoluto de una fraccion con el metodo abs" do
-        negativa = Racional.new(-3,4)
-        negativa.abs.should == Racional.new(3, 4)
+        Racional.new(-3,4).abs.should == Racional.new(3, 4)
 	end
 	
 	it "Se debe calcular el reciproco de una fraccion con el metodo reciprocal" do
@@ -50,52 +50,42 @@ describe Racional do
 	end
 	
 	it "Se debe calcular el opuesto de una fraccion con -" do
-        negativo = - @racional3
-		negativo.should == Racional.new(-4, 3)
+        (- @racional3).should == Racional.new(-4, 3)
 	end
 	
 	it "Se debe sumar dos fracciones con + y dar el resultado de forma reducida" do
-		resultado = @racional + Racional.new(1,2)
-        resultado.should == Racional.new(1,2)
+		(@racional + Racional.new(1,2)).should == Racional.new(1,2)
 	end
 	
 	it "Se debe restar dos fracciones con - y dar el resultado de forma reducida" do
-        resultado = @racional - Racional.new(1,2)
-        resultado.should == Racional.new(-1,2)
+        (@racional - Racional.new(1,2)).should == Racional.new(-1,2)
 	end
 	
 	it "Se debe multiplicar dos fracciones con * y dar el resultado de forma reducida" do
-        resultado = @racional * Racional.new(1,2)
-        resultado.should == Racional.new(0,2)
+        (@racional * Racional.new(1,2)).should == Racional.new(0,2)
 	end
 	
 	it "Se debe dividir dos fracciones con / y dar el resultado de forma reducida" do
-        resultado = @racional / Racional.new(1,2)
-        resultado.should == Racional.new(0,1)
+        (@racional / Racional.new(1,2)).should == Racional.new(0,1)
 	end
 	
 	it "Se debe calcular el resto dos fracciones con % y dar el resultado de forma reducida" do
-        resultado = @racional % Racional.new(1,2)
-        resultado.should == Racional.new(0,2)
+        (@racional % Racional.new(1,2)).should == Racional.new(0,2)
 	end
 	
 	it "Se debe de poder comprobar si una fracion es menor que otra" do
-		resultado = @racional < Racional.new(1,2)
-        resultado.should == true
+        (@racional < Racional.new(1,2)).should == true
 	end
 	
 	it "Se debe de poder comprobar si una fracion es mayor que otra" do
-		resultado = @racional > Racional.new(1,2)
-        resultado.should == false
+		(@racional > Racional.new(1,2)).should == false
 	end
 	
 	it "Se debe de poder comprobar si una fracion es menor o igual que otra" do
-		resultado = @racional <= Racional.new(1,2)
-        resultado.should == true
+        (@racional <= Racional.new(1,2)).should == true
 	end
 	
 	it "Se debe de poder comprobar si una fracion es mayor o igual que otra" do
-		resultado = @racional >= Racional.new(1,2)
-        resultado.should == false
+		(@racional >= Racional.new(1,2)).should == false
 	end
 end
